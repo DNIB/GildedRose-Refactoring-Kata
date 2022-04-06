b02345f9b6e598dbca1a069902145b20d9837ba2
DNIB <qazs0205@gmail.com>
2022-04-07 00:30:13 +0800

    Refactor update of normal item

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -48,16 +48,13 @@ final class GildedRose
                     return $item;
                 }
 
-                if ($item->quality > 0) {
-                    $item->quality = $item->quality - 1;
-                }
-    
-                $item->sell_in = $item->sell_in - 1;
-    
-                if ($item->sell_in < 0) {
-                    if ($item->quality > 0) {
-                        $item->quality = $item->quality - 1;
-                    }
+                $item->sell_in -= 1;
+                $item->quality -= ($item->sell_in < 0) ? 2 : 1;
+
+                if ($item->quality > 50) {
+                    $item->quality = 50;
+                } elseif ($item->quality < 0) {
+                    $item->quality = 0;
                 }
 
                 return $item;
