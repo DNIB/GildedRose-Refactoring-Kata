852786f6daa9b232c363ab848c123cdf7a1c8ff2
DNIB <qazs0205@gmail.com>
2022-04-07 00:06:22 +0800

    Early return the result of Hand Of Ragnaros

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -20,11 +20,13 @@ final class GildedRose
     {
         $this->items = array_map(
             function (Item $item): Item {
+                if ($item->name === 'Sulfuras, Hand of Ragnaros') {
+                    return $item;
+                }
+
                 if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                     if ($item->quality > 0) {
-                        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                            $item->quality = $item->quality - 1;
-                        }
+                        $item->quality = $item->quality - 1;
                     }
                 } else {
                     if ($item->quality < 50) {
@@ -44,17 +46,13 @@ final class GildedRose
                     }
                 }
     
-                if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                    $item->sell_in = $item->sell_in - 1;
-                }
+                $item->sell_in = $item->sell_in - 1;
     
                 if ($item->sell_in < 0) {
                     if ($item->name != 'Aged Brie') {
                         if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                             if ($item->quality > 0) {
-                                if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                    $item->quality = $item->quality - 1;
-                                }
+                                $item->quality = $item->quality - 1;
                             }
                         } else {
                             $item->quality = $item->quality - $item->quality;
