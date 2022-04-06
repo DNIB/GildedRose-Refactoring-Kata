ac0ceabe9880afe4aa1d2e0b832cd878752e3bb0
DNIB <qazs0205@gmail.com>
2022-04-07 00:16:55 +0800

    Early return the result of Aged Brie

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -24,7 +24,15 @@ final class GildedRose
                     return $item;
                 }
 
-                if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                if ($item->name === 'Aged Brie') {
+                    $item->sell_in -= 1;
+                    
+                    $item->quality += ($item->sell_in >= 0) ? 1 : 2;
+                    $item->quality = ($item->quality <= 50) ? $item->quality : 50;
+                    return $item;
+                }
+
+                if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                     if ($item->quality > 0) {
                         $item->quality = $item->quality - 1;
                     }
@@ -49,18 +57,12 @@ final class GildedRose
                 $item->sell_in = $item->sell_in - 1;
     
                 if ($item->sell_in < 0) {
-                    if ($item->name != 'Aged Brie') {
-                        if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                            if ($item->quality > 0) {
-                                $item->quality = $item->quality - 1;
-                            }
-                        } else {
-                            $item->quality = $item->quality - $item->quality;
+                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                        if ($item->quality > 0) {
+                            $item->quality = $item->quality - 1;
                         }
                     } else {
-                        if ($item->quality < 50) {
-                            $item->quality = $item->quality + 1;
-                        }
+                        $item->quality = $item->quality - $item->quality;
                     }
                 }
 
