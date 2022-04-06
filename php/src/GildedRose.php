f1bc0be30b3a7d318173de7ea158b79f6d6c4812
DNIB <qazs0205@gmail.com>
2022-04-07 00:23:19 +0800

    Early return the result of Backstage passes

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -26,43 +26,37 @@ final class GildedRose
 
                 if ($item->name === 'Aged Brie') {
                     $item->sell_in -= 1;
-                    
+
                     $item->quality += ($item->sell_in >= 0) ? 1 : 2;
                     $item->quality = ($item->quality <= 50) ? $item->quality : 50;
                     return $item;
                 }
 
-                if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($item->quality > 0) {
-                        $item->quality = $item->quality - 1;
+                if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
+                    $item->sell_in -= 1;
+
+                    if ($item->sell_in > 10) {
+                        $item->quality += 1;
+                    } elseif ($item->sell_in > 5) {
+                        $item->quality += 2;
+                    } elseif ($item->sell_in > 0) {
+                        $item->quality += 3;
+                    } else {
+                        $item->quality = 0;
                     }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                        if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                            if ($item->sell_in < 11) {
-                                if ($item->quality < 50) {
-                                    $item->quality = $item->quality + 1;
-                                }
-                            }
-                            if ($item->sell_in < 6) {
-                                if ($item->quality < 50) {
-                                    $item->quality = $item->quality + 1;
-                                }
-                            }
-                        }
-                    }
+                    $item->quality = ($item->quality <= 50) ? $item->quality : 50;
+                    return $item;
+                }
+
+                if ($item->quality > 0) {
+                    $item->quality = $item->quality - 1;
                 }
     
                 $item->sell_in = $item->sell_in - 1;
     
                 if ($item->sell_in < 0) {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            $item->quality = $item->quality - 1;
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
+                    if ($item->quality > 0) {
+                        $item->quality = $item->quality - 1;
                     }
                 }
 
